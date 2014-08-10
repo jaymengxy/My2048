4c399c27eeab418be291c16519987b967fd609d5
SinTi <jaymengsniper@gmail.com>
2014-08-10 22:50:43 +0800

    自定义GameView

---
+++ src/com/mxy/My2048/GameView.java
@@ -0,0 +1,24 @@
+package com.mxy.My2048;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by SinTi on 2014/8/9.
+ *
+ */
+public class GameView extends LinearLayout{
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
--- src/com/mxy/My2048/MyActivity.java
+++ src/com/mxy/My2048/MyActivity.java
@@ -2,6 +2,7 @@ package com.mxy.My2048;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +12,8 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void restart(View view) {
     }
 }
